6af7421fc4ff2ef225dc3ed6da9da70a17f9d99b
AkilaMihiranga123 <akilamihiranganew475@gmail.com>
2022-04-10 13:48:13 +0530

    branch-custmor-v1-update item quantity in cart functionality added

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($row_id)
+    {
+        $product = Cart::get($row_id);
+        $qty = $product->qty + 1;
+        Cart::update($row_id, $qty);
+    }
+
+    public function decreaseQuantity($row_id)
+    {
+        $product = Cart::get($row_id);
+        $qty = $product->qty - 1;
+        Cart::update($row_id, $qty);
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
