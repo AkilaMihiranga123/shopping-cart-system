743cb001c5aeccc823a5a63ff9356c1afd7f4b34
Piyumi14 <lbpmdayarathna@gmail.com>
2022-04-12 12:35:59 +0530

    branch-admin-v1 - List All Products in Admin Panel - v_17

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -76,7 +76,10 @@
                                                     <a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
                                                 </li>
                                                 <li class="menu-item" >
-                                                    <a title="Categories" href="{{ route('admin.categories') }}">Category</a>
+                                                    <a title="Categories" href="{{ route('admin.categories') }}">Categories</a>
+                                                </li>
+                                                <li class="menu-item" >
+                                                    <a title="Products" href="{{ route('admin.products') }}">Products</a>
                                                 </li>
                                                 <li class="menu-item" >
                                                     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailComponent;
@@ -44,4 +45,5 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
     Route::get('/admin/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
+    Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
 });
