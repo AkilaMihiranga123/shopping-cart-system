253c38dd731cc57186e60c9886573d10163f3475
AkilaMihiranga123 <akilamihiranganew475@gmail.com>
2022-04-13 19:01:56 +0530

    branch-custmor-v1-auto refresh wishlist and cart count

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -12,6 +12,7 @@ class CartComponent extends Component
         $product = Cart::instance('cart')->get($row_id);
         $qty = $product->qty + 1;
         Cart::instance('cart')->update($row_id, $qty);
+        $this->emitTo('cart-count-component', 'refreshComponent');
     }
 
     public function decreaseQuantity($row_id)
@@ -19,17 +20,20 @@ class CartComponent extends Component
         $product = Cart::instance('cart')->get($row_id);
         $qty = $product->qty - 1;
         Cart::instance('cart')->update($row_id, $qty);
+        $this->emitTo('cart-count-component', 'refreshComponent');
     }
 
     public function destroy($row_id)
     {
         Cart::instance('cart')->remove($row_id);
+        $this->emitTo('cart-count-component', 'refreshComponent');
         session()->flash('success_message', 'Item has been removed');
     }
 
     public function destroyAll()
     {
         Cart::instance('cart')->destroy();
+        $this->emitTo('cart-count-component', 'refreshComponent');
     }
 
     public function render()
