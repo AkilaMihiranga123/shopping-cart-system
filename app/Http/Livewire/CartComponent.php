10170ab601b1c01d4982e9228c34288174d65e1e
AkilaMihiranga123 <akilamihiranganew475@gmail.com>
2022-04-13 15:41:49 +0530

    branch-custmor-v1-add product to wishlist and update cart component

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -9,27 +9,27 @@ class CartComponent extends Component
 {
     public function increaseQuantity($row_id)
     {
-        $product = Cart::get($row_id);
+        $product = Cart::instance('cart')->get($row_id);
         $qty = $product->qty + 1;
-        Cart::update($row_id, $qty);
+        Cart::instance('cart')->update($row_id, $qty);
     }
 
     public function decreaseQuantity($row_id)
     {
-        $product = Cart::get($row_id);
+        $product = Cart::instance('cart')->get($row_id);
         $qty = $product->qty - 1;
-        Cart::update($row_id, $qty);
+        Cart::instance('cart')->update($row_id, $qty);
     }
 
     public function destroy($row_id)
     {
-        Cart::remove($row_id);
+        Cart::instance('cart')->remove($row_id);
         session()->flash('success_message', 'Item has been removed');
     }
 
     public function destroyAll()
     {
-        Cart::destroy();
+        Cart::instance('cart')->destroy();
     }
 
     public function render()
