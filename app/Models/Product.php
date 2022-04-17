1dd4079b0a379ac16195dc6ade995e3af15e4091
AkilaMihiranga123 <akilamihiranganew475@gmail.com>
2022-04-18 01:33:21 +0530

    branch-custmor-v1-show review and rating on product details page

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,13 @@ class Product extends Model
 
     protected $table = "products";
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class,'category_id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'product_id');
     }
 }
